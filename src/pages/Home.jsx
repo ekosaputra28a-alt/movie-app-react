a221de505fa6246ea1ab187c5b3057d29b6c8906
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2026-02-24 22:12:37 +0700

    pesan commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,38 @@
 import MovieCard from "../components/MovieCard";
 import "../styles/movie.css";
 
+const topRatedMovies = [
+  {
+    id: 6,
+    title: "Joker",
+    rating: 8.3,
+    image: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+  },
+  {
+    id: 7,
+    title: "Spider-Man: No Way Home",
+    rating: 8.2,
+    image: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+  },
+  {
+    id: 8,
+    title: "Top Gun: Maverick",
+    rating: 8.3,
+    image: "https://image.tmdb.org/t/p/w500/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
+  },
+  {
+    id: 9,
+    title: "The Shawshank Redemption",
+    rating: 9.3,
+    image: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  },
+  {
+    id: 10,
+    title: "The Batman",
+    rating: 7.8,
+    image: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+  },
+ ];
 const dummyMovies = [
   {
     id: 1,
@@ -26,25 +58,39 @@ const dummyMovies = [
     rating: 9.0,
     image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
   },
+  {
+    id: 5,
+    title: "Parasite",
+    rating: 8.5,
+    image: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+  },
+  
 ];
 
 function Home() {
   return (
     <main>
-      <h2 style={{ color: "white", marginBottom: "16px" }}>
-        Popular Movies
-      </h2>
 
-      <div className="movie-grid">
-        {dummyMovies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            rating={movie.rating}
-            image={movie.image}
-          />
-        ))}
-      </div>
+      {/* Popular */}
+      <section>
+        <h2 className="section-title">Popular Movies</h2>
+        <div className="movie-grid">
+          {dummyMovies.map((movie) => (
+            <MovieCard key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+
+      {/* Top Rated */}
+      <section>
+        <h2 className="section-title">Top Rated</h2>
+        <div className="movie-grid">
+          {topRatedMovies.map((movie) => (
+            <MovieCard key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+
     </main>
   );
 }
