1a1dea815089e982e3c1659739aeb9fe1eea277a
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2026-02-24 17:06:49 +0700

    pesan commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,15 @@ function Navbar() {
   return (
     <nav className="navbar">
       <h1>MovieApp</h1>
+      <ul className="nav-links">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>Popular</li>
+      </ul>
       <input type="text" placeholder="Search movie..." />
+
+      
     </nav>
   );
 }
