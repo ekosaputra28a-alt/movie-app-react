4c73e1e6e407614f3dc9323cfce1eadb0401d73f
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2026-02-25 16:08:02 +0700

    pesan commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,21 @@ import "../styles/navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>MovieApp</h1>
-      <ul className="nav-links">
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Popular</li>
-      </ul>
-      <input type="text" placeholder="Search movie..." />
+      <h1 className="logo">MovieApp</h1>
 
-      
+      {/* NAV LINKS di tengah */}
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>TV Shows</li>
+        <li>Movies</li>
+        <li>Popular</li>
+      </ul>
+
+      {/* Kanan: Search + Settings */}
+      <div className="nav-right">
+        <input type="text" placeholder="Search movie..." />
+        <button className="navbar-settings" title="Settings">⚙️</button>
+      </div>
     </nav>
   );
 }
